c4554c4715c327218a51448bfbd7ffcfbcb750d5
Joan-R0driguez <Joan-R0driguez@users.noreply.github.com>
2021-07-15 18:25:16 +0000

    deploy: be319bac3b02532d8dc267e3f6b5bfede294008c

---
+++ _dist_/index.js
@@ -0,0 +1,34 @@
+import registerImage from "./Lazy.js"
+
+const maximum = 122;
+const minimum = 1;
+
+const random = () => 
+    Math.floor(Math.random() * (maximum - minimum)) + minimum;
+
+const createImageNode = () => {
+    const container = document.createElement("div");
+    container.className = "p-4";
+
+    const imagen = document.createElement("img");
+    imagen.className = "mx-auto";
+    imagen.width = "320";
+    imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+    container.appendChild(imagen);
+
+    return container;
+}
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById("images")
+
+const addButton = document.querySelector('button');
+
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage);
+    registerImage(newImage);
+}
+
+addButton.addEventListener("click", addImage);
